6150ad6149fe7a0510fcfa15babae2936d496479
jorgeo80 <jorgeo80@gmail.com>
2024-04-29 19:30:22 -0600

    [ADD] Pruebas-Jest

--- 03-counter-app-vite/src/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/src/tests/FirstApp.test.jsx
@@ -5,7 +5,11 @@ describe('Pruebas en FirstApp', () => {
     test('debe hacer match con el snapshot', () => {
         
         const title = 'Hola, soy Mundo';
-        render( <FirstApp title={title}/> );
+        const { container, getByText } = render( <FirstApp title={title}/> );
+        expect( getByText ).toBeTruthy();
+
+        // const h1 = container.querySelector('h1');
+        // expect(h1.innerHTML).toContain(title);
 
     });
   
