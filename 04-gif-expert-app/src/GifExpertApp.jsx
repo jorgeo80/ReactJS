233cac76a5e7ad871fa46d46c16fc760c495474f
jorgeo80 <jorgeo80@gmail.com>
2024-04-29 20:44:35 -0600

    [ADD] Styles

---
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,17 @@
+
+export const GifExpertApp = () => {
+  return (
+    <>
+      
+      {/* Titulo */}
+      <h1>Gif Expert App</h1>
+      
+      {/* Input */}
+      
+      {/* Listado de Gifs */}
+      
+      {/* Gif Item */}
+
+    </>
+  )
+}
